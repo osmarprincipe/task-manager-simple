aa0bffc07f6fa4b44669ade2b9c82e01e7c2b41f
osmar <osmarprincipe98@gmail.com>
2026-02-04 14:50:26 +0000

    Fix duplicated JS logic2

--- script-1.js
+++ script-1.js
@@ -1,11 +1,4 @@
 const btn = document.getElementById("completeBtn");
-
-btn.addEventListener("click", () => {
-  alert("Task completed! Notification sent.");
-});
-
-
-const btn = document.getElementById("completeBtn");
 const message = document.getElementById("message");
 
 btn.addEventListener("click", () => {
