d508dded918472b97476857873c905d792968979
osmar <osmarprincipe98@gmail.com>
2026-02-04 14:23:59 +0000

    Show notification message on page

--- script-1.js
+++ script-1.js
@@ -3,3 +3,12 @@ const btn = document.getElementById("completeBtn");
 btn.addEventListener("click", () => {
   alert("Task completed! Notification sent.");
 });
+
+
+const btn = document.getElementById("completeBtn");
+const message = document.getElementById("message");
+
+btn.addEventListener("click", () => {
+  alert("Task completed! Notification sent.");
+  message.textContent = "Task completed! Notification sent.";
+});
